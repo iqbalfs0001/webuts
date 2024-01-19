a61fa2fd12cb0cc4fbd0934e2a69d377f0c843f6
Ilyas <ilyiyas05@gmail.com>
2024-01-19 19:38:43 +0800

    IniIlyasPart2

--- ilyas.php
+++ ilyas.php
@@ -1,1 +1,39 @@
-Halo ini ilyas
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width= , initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<form>
+  <label for="fname">Nama Lengkap :</label>
+  <input type="text"><br>
+  <label>Jenis kelamin : <select>
+    <option>Laki-Laki</option>
+    <option value="">Perempuan</option>
+  </select></label><br>
+  <label for="fname">Alamat :</label>
+  <input type="text"><br>
+  <label for="fname">Tanggal Lahir</label>
+  <input type="date"><br>
+  <label>Lulus Sekolah :</label>
+  <input type="radio" value="lulus" name="lulus"> Lulus
+  <input type="radio" value="tidak lulus" name="lulus"> Tidak Lulus
+  <br>
+  <label for="fname">Hobby :</label>
+  <input type="Checkbox"> Berenang 
+  <input type="Checkbox"> Belajar
+  <input type="Checkbox"> Berkebun 
+  <input type="Checkbox"> Bertani
+  <br> 
+  <label for="">Warna Favorite</label>
+  <input type="color">
+  <br>
+  <label for="">Masukkan foto</label>
+  <input type="file">
+  <br>
+  <input type="submit">
+</form>
+</body>
+</html>
