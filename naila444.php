761512fa3b0f80a63eddc64c41b8188a37e2533e
nel4z0wie <salsabilah.bp05@gmail.com>
2024-01-19 03:01:21 -0800

    file naila berubah

--- naila444.php
+++ naila444.php
@@ -1,1 +1,17 @@
-hallo ini naila 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<body>
+<h2>HTML Forms</h2>
+<form action="/action_file page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+</head>
+<body>
+    
+</body>
+</html>
