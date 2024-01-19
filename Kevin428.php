1e0b0335ac5cf43072ea01cb7d58e9e417b67061
Kevin420 <kevinpalulungan428@gmail.com>
2024-01-19 19:30:03 +0800

    FIle Kevin diubah

--- Kevin428.php
+++ Kevin428.php
@@ -10,11 +10,24 @@
     <form action="/action_page.php">
   <label for="fname">First name:</label><br>
   <input type="text" id="fname" name="fname" value=""><br>
+  <label for="jenis kelamin">Jenis Kelamin:
+    <select name="" id="">
+        <option value="">laki-laki</option>
+        <option value="">Perempuan</option>
+        <option value="">Ladyboy</option>
+  </label><br><br>
   <label for="lname">Last name:</label><br>
   <input type="text" id="lname" name="lname" value=""><br><br>
   <label for="alamat">Alamat:</label><br>
   <input type="text" id="alamat" name="alamat" value=""><br>
   <label for=""> Tanggal Lahir</label><br>
+  <label for="hobby">Hobby:</label><br>
+  <input type="checkbox" id="basket1" name="basket1" value="basket">
+  <label for="basket1"> Main Basket</label><br>
+  <input type="checkbox" id="game2" name="game2" value="Game">
+  <label for="game2"> Main Game</label><br>
+  <input type="checkbox" id="Tidur3" name="tidur3" value="tidur">
+  <label for="tidur3"> Tidur</label>
   <input type="date" value=""><br>
   <input type="submit" value="Submit">
 </form> 
