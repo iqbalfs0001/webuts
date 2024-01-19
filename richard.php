6eb89864e6c5b87716c7d05b26632385448b2a4e
ZEIN725 <zefnonerchd@gmail.com>
2024-01-19 19:37:42 +0800

    Berkelas

--- richard.php
+++ richard.php
@@ -1,1 +1,44 @@
-Richard
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DOK</title>
+</head>
+
+<body>
+
+    <form action="">
+        <label for="">Nama Lengkap
+            <input type="text" name="" id="">
+        </label>
+    <br>
+    <label for="">Jenis Kelamin</label>    
+    <select name="" id="">
+        <option value="">Laki-laki</option>
+        <option value="">Perempuan</option>
+    </select>
+<br>
+<label for="">
+    alamat <textarea name="" id="" cols="30" rows="10"></textarea>
+</label>
+
+<br>
+<label for="">Tanggal lahir
+    <input type="date" name="" id="">
+</label>
+<br>
+<label for="">Hobby
+    <input type="checkbox" name="" id=""> Berenang
+    <input type="checkbox" name="" id=""> Belajar
+    <input type="checkbox" name="" id=""> Berkebun
+    <input type="checkbox" name="" id=""> Bertani
+</label>
+<br>
+<button>Simpan</button>
+</form>
+
+
+</body>
+</html>
