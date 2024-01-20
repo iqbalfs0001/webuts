ae5bffd1b805d626312035ffc554a9a43fae5b5a
Irunnn16 <khoirunnugraha145@gmail.com>
2024-01-20 13:57:07 +0800

    ini punya irunnn

--- khoirun nugraha.php
+++ khoirun nugraha.php
@@ -1,1 +1,52 @@
-halo ini irunnn16 
+<form action="">
+    <label for="namalengkap">
+        nama lengkap
+        <input type="text" id="namalengkap" placeholder="tuliskan nama lengkap anda">
+    </label>
+    <br>
+    <label for="">
+        jenis kelamin
+        <select name="" id="">
+            <option value="">laki laki</option>
+            <option value="">Perempuan</option>
+        </select>
+    </label>
+    <br>
+    <label for="">
+        alamat
+        <textarea name="" id="" ></textarea>
+    </label>
+    <br>
+    
+    <label for="">
+        Tanggal lahir
+        <input type="date">
+    </label>
+
+    <br>
+    lulus sekolah :
+    <label for=""><input type="radio" value="lulus" name="lulus" checked> sudah lulus</label>
+    <label for=""><input type="radio" value="tidak lulus" name="lulus"> belum lulu</label>
+    <br>
+    hobby :
+    <label for="hobyberenang"><input type="checkbox" name="hoby" value="berenang" id="hobyberenang"> berenang</label>
+    <label for=""><input type="checkbox" name="hoby" value="belajar" checked> belajar</label>
+    <label for=""><input type="checkbox" name="hoby" value="berkebun"> berkebun</label>
+    <label for=""><input type="checkbox" name="hoby" value="bertani"> bertani</label>
+
+    <br>
+
+    <label for="">
+        warna favourite
+        <input type="color">
+    </label>
+    <br>
+    <label for="">
+        upload photo
+        <input type="file">
+    </label>
+    <br>
+
+    <button>simpan</button>
+
+</form>
